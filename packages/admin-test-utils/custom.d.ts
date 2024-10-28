bcb5ef7dc8e70b01da86ce8c13054cdb67acfaa6
Owen Rossi-Keen <owenrossikeen@gmail.com>
2024-10-27 21:53:27 -0400

    Initial Commit

---
+++ packages/admin-test-utils/custom.d.ts
@@ -0,0 +1,19 @@
+export {};
+
+declare global {
+  interface Window {
+    strapi: {
+      backendURL: string;
+      isEE: boolean;
+      features: {
+        SSO: 'sso';
+        isEnabled: (featureName?: string) => boolean;
+      };
+      projectType: string;
+      telemetryDisabled: boolean;
+      flags: {
+        nps: boolean;
+      };
+    };
+  }
+}
